e79011bf34e17de048f806304b41e7235c1e606c
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-09 02:42:15 +0100

    Update handlerfunctions3.c

--- handlerfunctions3.c
+++ handlerfunctions3.c
@@ -48,6 +48,8 @@ void rotr_handler(stack_t **stack, unsigned int line_number)
  */
 void stack_handler(stack_t **stack, unsigned int line_number)
 {
+	data_t data = DATA_INIT;
+
 	(void)stack;
 	(void)line_number;
 	data.qflag = 0;
@@ -61,6 +63,8 @@ void stack_handler(stack_t **stack, unsigned int line_number)
  */
 void queue_handler(stack_t **stack, unsigned int line_number)
 {
+	data_t data = DATA_INIT;
+
 	(void)stack;
 	(void)line_number;
 	data.qflag = 1;
